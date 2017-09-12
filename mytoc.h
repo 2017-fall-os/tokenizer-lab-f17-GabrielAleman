a17d52973dff059ad0a2103452340845fc330b9a
Gabriel Aleman <galeman2@hotmail.com>
2017-09-11 23:54:00 -0600

    Delete mytoc.h

--- mytoc.h
+++
@@ -1,6 +0,0 @@
-
-char ** lengthOfWord(char * string, char ** wordArray,char delim);
-int wordCounter(char * string,char delim);
-char ** mytoc(char * string,char delim);
-void freeMem(char * string, char ** wordArray,char delim);
-void printArray(char * sting,char ** wordArray,char delim);
