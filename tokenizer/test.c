7ddcfa47db17232bfe54beef052c2c6708b9e11d
Gabriel Aleman <galeman2@hotmail.com>
2017-12-07 09:30:30 -0700

    clean test.c

--- tokenizer/test.c
+++ tokenizer/test.c
@@ -5,31 +5,14 @@
 #include "test.h"
 
 int main(){
-   char ** array;
+  char ** array;
   char temp [100];
   char string1 [] = "Hello my dog's name is Darwin\n";
   char string2 [] = "   Hello  World    \n";
   char string3 [] = "\n";
   char string4 [] = "         \n";
   char string5 [] = " HelloWorld\n";
-  int loop = 1;
-  char delim  [1];
-  char notice [] = "pick Delim\n";
-  //write(1,notice,11);
-  //read(0,delim,1);
-  
-  /*char string []= "type 'exit'to exit\n";
-  write(1,string,19);
-  while(loop){
-    char a []= "$";
-    write(1,a,2);
-    read(0,temp,100);
-    loop = compareString(temp);
-    array = mytoc(temp,delim[0]);
-    printArray(temp,array,delim[0]);
-    freeMem(temp, array,delim[0]);
-  }
-  */  array = mytoc(string1,' ');
+  array = mytoc(string1,' ');
   printArray(string1,array,' ');
   freeMem(string1,array,' ');
   printf("\n");
@@ -55,22 +38,6 @@ int main(){
   
 }
 
-int compareString(char * string){
-  int loop = 0;
-  int compare = 0;
-  char terminator [] = "exit";
-  while(string[loop] != '\n' && loop < 4){
-    if(string[loop] == terminator[loop]){
-      compare++;
-    }
-    loop++;
-  }
-  if(compare == 4){
-    return 0;
-  }
-  else{
-    return 1;
-  }
-}
+
     
      
